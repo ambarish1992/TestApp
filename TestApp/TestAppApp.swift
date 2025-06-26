ee9595e007f25b2b99cdf9e84db29a698de530d4
ambarish1992 <ambarish.shivakumar1992@gmail.com>
2025-06-26 10:58:41 +0530

    Initial Commit

---
+++ TestApp/TestAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestAppApp.swift
+//  TestApp
+//
+//  Created by Ambarish Shivakumar on 26/06/25.
+//
+
+import SwiftUI
+
+@main
+struct TestAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
